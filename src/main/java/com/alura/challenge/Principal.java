d69c9775c607bfeea63746fa4ea68b0049839e9b
Jair Coral <jaircoral88@gmail.com>
2024-07-03 21:31:26 -0500

    Se inicia repo se importa Gson Se crea clase principal se Crea clase moneda con sus propieades

---
+++ src/main/java/com/alura/challenge/Principal.java
@@ -0,0 +1,15 @@
+package com.alura.challenge;
+import com.google.gson.Gson;
+
+public class Principal {
+    public static void main(String[] args){
+        // Verificar la configuración inicial imprimiendo un mensaje en JSON usando Gson
+        Gson gson = new Gson();
+        String message = gson.toJson("Hola, mundo!");
+        System.out.println(message);
+
+        // Aquí puedes empezar a implementar la lógica de tu conversor de monedas
+        // Por ejemplo, podrías comenzar llamando a una API de conversión de monedas
+        // y procesando los datos recibidos.
+    }
+}
